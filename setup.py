5913b0885c0b514c941d9b068b12fbfcb7857365
Atul <anamikagolu1508@gmail.com>
2023-01-25 06:38:40 +0530

    setup of local file

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import setuptools
 
 with open("README.md","r",encoding="utf-8") as f:
@@ -9,3 +8,20 @@ __version__="0.0.0"
 REPO_NAME="template-setup-1.0"
 AUTHOR_USER_NAME= "C17hawke"
 SRC_REPO="template-setup-1.0"
+AUTHOR_EMAIL="anamikagolu1508@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author_mail=AUTHOR_EMAIL,
+    author_user_name=AUTHOR_USER_NAME,
+    description="A small python package",
+    long_description=long_description,
+    long_description="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "bug tracker":f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"scr"},
+    packages=setuptools.find_packages(where="src")
+)
